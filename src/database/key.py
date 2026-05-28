3b7b92404aafa6d3834d2a11c91b2cefa14ae446
Jacques Donald ZAMBLE <jackdo@Ordinateur-portable-de-Jacques.local>
2026-05-28 15:31:51 +0200

    feat(data): scripts d'ingestion RAW et d'ETL SQL pour la couche ENRICHED

--- src/database/key.py
+++
@@ -1,7 +0,0 @@
-from google.cloud import bigquery
-
-PROJECT_ID = "bigquery-finance-ia"
-path_to_json = "/Users/jackdo/Downloads/bigquery-finance-ia-1972976e09a1.json"
-
-# On initialise le client en lui fournissant directement la clé JSON
-client = bigquery.Client.from_service_account_json(path_to_json, project=PROJECT_ID)
